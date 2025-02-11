da5bfb03a3a62268b339ceaad361aebd62e5b71f
Radian0523 <daichi200505230523@gmail.com>
2025-02-11 14:27:40 +0900

    update：講義85まで

---
+++ Galaxy Strike/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    [SerializeField] float controlSpeed = 10f;
+    Vector2 movement;
+    [SerializeField] float xClampRange = 10f;
+    [SerializeField] float yClampRange = 10f;
+
+    [SerializeField] float controlPitchFactor = 10f;
+    [SerializeField] float controlRollFactor = 10f;
+    [SerializeField] float controlRotationSpeed = 10f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+        //Debug.Log(movement);
+    }
+
+
+    public void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+
+    void ProcessRotation()
+    {
+        float pitch = -controlPitchFactor * movement.y;
+        float roll = -controlRollFactor * movement.x;
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, controlRotationSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] GameObject[] lasers;
+    bool isFiring;
+    void Update()
+    {
+        ProcessFiring();
+    }
+    public void OnFire(InputValue value)
+    {
+        isFiring = value.isPressed;
+    }
+
+    void ProcessFiring()
+    {
+
+        foreach (GameObject laser in lasers)
+        {
+            var emissionModule = laser.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isFiring;
+        }
+
+    }
+
+}
